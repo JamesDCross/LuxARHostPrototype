86e5e6a6a73fe7e286ad877e1096bf0545671a40
Greg Whitfield [DATACOM] <Gregw@datacom.co.nz>
2018-11-30 15:59:19 +1300

    Created custom Network Manager

---
+++ Assets/Scripts/LuxNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LuxNetworkManager : NetworkManager {
+
+    public void OnClientConnect(UnityEngine.Networking.NetworkConnection conn)
+    {
+        //Your custom player spawning logic
+        //Decide here who is a zombie and who is a human, etc.
+
+        Debug.Log("Client connect");
+
+        base.OnClientConnect(conn);
+
+    }
+}
