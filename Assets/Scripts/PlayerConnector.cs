a54dfb4ca9ff2c86ce4193ed7b98818c79ea5245
Greg Whitfield <grwhitfield@gmail.com>
2019-01-26 23:25:52 +1300

    Lines now get destroyed, and NPC's work same as players

--- Assets/Scripts/PlayerConnector.cs
+++ Assets/Scripts/PlayerConnector.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerConnector : NetworkBehaviour {
+public class PlayerConnector : NetworkBehaviour
+{
 
     public GameObject lineConnectorPrefab;
     public Transform lineTransform;
 
+    Dictionary<Collider, GameObject> lineMap;
+
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        lineMap = new Dictionary<Collider, GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -30,33 +35,37 @@ public class PlayerConnector : NetworkBehaviour {
         }
 
         // Instantiate a line connector
-
         if (lineConnectorPrefab != null)
         {
-            var l = (GameObject)Instantiate(lineConnectorPrefab, Vector3.zero, Quaternion.identity);
-            
-            l.GetComponent<LineConnector>().end1 = transform;
-            l.GetComponent<LineConnector>().end2 = other.transform;
-            Debug.Log("Connector created" + l);
-        }
+            if (!lineMap.ContainsKey(other))
+            {
+                // Only create a line if we are not already linked to this object
+                var l = (GameObject)Instantiate(lineConnectorPrefab, Vector3.zero, Quaternion.identity);
 
-        PlayerMove pm = GetComponent<PlayerMove>();
-        if (pm)
-        {
-            pm.SetTrailColor(Color.blue);
-        }
-        else
-        {
-            AutoNPCMotion am = GetComponent<AutoNPCMotion>();
-            if (am)
-            {
-                am.SetTrailColor(Color.blue);
-            }
-            else
-            {
-                Debug.LogError("Neither player nor NPC found!!");   
+                l.GetComponent<LineConnector>().end1 = transform;
+                l.GetComponent<LineConnector>().end2 = other.transform;
+                Debug.Log("Connector created" + l);
+
+                lineMap.Add(other, l);
             }
         }
+        //PlayerMove pm = GetComponent<PlayerMove>();
+        //if (pm)
+        //{
+        //    pm.SetTrailColor(Color.blue);
+        //}
+        //else
+        //{
+        //    AutoNPCMotion am = GetComponent<AutoNPCMotion>();
+        //    if (am)
+        //    {
+        //        am.SetTrailColor(Color.blue);
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("Neither player nor NPC found!!");   
+        //    }
+        //}
     }
 
     private void OnTriggerExit(Collider other)
@@ -64,25 +73,33 @@ public class PlayerConnector : NetworkBehaviour {
         // Debug.Log("Objects parted");
         if (Equals(other.name, "Arena"))
         {
-            Debug.Log("Arena hit");
+            Debug.Log("Arena exit");
             return;
         }
-        PlayerMove pm = GetComponent<PlayerMove>();
-        if (pm)
+
+        // Find line previously connected   
+        if (lineMap.ContainsKey(other))
         {
-            pm.SetTrailColor(Color.white);
+            Destroy(lineMap[other]);
+            lineMap.Remove(other);
         }
-        else
-        {
-            AutoNPCMotion am = GetComponent<AutoNPCMotion>();
-            if (am)
-            {
-                am.SetTrailColor(Color.white);
-            }
-            else
-            {
-                Debug.LogError("Neither player nor NPC found!!");
-            }
-        }
+
+        //PlayerMove pm = GetComponent<PlayerMove>();
+        //if (pm)
+        //{
+        //    pm.SetTrailColor(Color.white);
+        //}
+        //else
+        //{
+        //    AutoNPCMotion am = GetComponent<AutoNPCMotion>();
+        //    if (am)
+        //    {
+        //        am.SetTrailColor(Color.white);
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("Neither player nor NPC found!!");
+        //    }
+        //}
     }
 }
