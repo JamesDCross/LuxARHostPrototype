478c72989c5acce9583b125b8eb93c614ae17958
Greg Whitfield <grwhitfield@gmail.com>
2019-02-14 23:33:45 +1300

    Added base framework for player settings in lobby being picked up by game.

---
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+    [SyncVar]
+    public string pname = "lux player";
+
+    [SyncVar]
+    public Color playerColor = Color.white;
+
+    [Command] 
+    public void CmdChangeName(string newName)
+    {
+        pname = newName;
+        //this.GetComponentInChildren<TextMesg>().text = pname;
+    }
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer)
+        {
+            // Set all components to chosen color
+            // NB: We proably just want to set flare colour
+           // Renderer[] rends = GetComponentInChildren<Renderer>();
+           // foreach (Renderer r in rends)
+           //     r.material.color = playerColor;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //this.GetComponentInChildren<TextMesg>().text = pname;
+    }
+}
