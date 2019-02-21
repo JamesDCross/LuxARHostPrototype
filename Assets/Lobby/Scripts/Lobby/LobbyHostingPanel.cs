b71131521bbd76ba94313b2815bdf8a02ec6f096
Greg Whitfield <grwhitfield@gmail.com>
2019-02-22 08:12:02 +1300

    Made hosting non-mobile only.
    Note that LobyManager no longer a prefab - can redo it later if needed.

---
+++ Assets/Lobby/Scripts/Lobby/LobbyHostingPanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyHostingPanel : MonoBehaviour {
+
+    protected Image panelImage;
+
+    // Use this for initialization
+    void Start () {
+
+        panelImage = GetComponent<Image>();
+
+        if (Application.isMobilePlatform) {
+            // Disable if running on moile platform
+
+            if (!panelImage) {
+                Debug.LogError("Could not find hosting subbpanel");
+                return;
+            }
+            SetVisibility(false);
+        }
+		
+	}
+
+    public void SetVisibility(bool visible)
+    {
+        foreach (Transform t in transform)
+        {
+            t.gameObject.SetActive(visible);
+        }
+
+        if (panelImage != null)
+        {
+            panelImage.enabled = visible;
+        }
+    }
+
+}
