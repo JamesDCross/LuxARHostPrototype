c5e37e16b5250ed4452e70163c0f881da64ee3de
Greg Whitfield [DATACOM] <Gregw@datacom.co.nz>
2019-02-24 23:41:47 +1300

    Added UI panel to player view - initial setup

---
+++ Assets/Scripts/PlayerMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMainMenu : MonoBehaviour {
+
+    //Main menu for player view, mainly only a bunch of callback called by the UI (setup throught the Inspector)
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
