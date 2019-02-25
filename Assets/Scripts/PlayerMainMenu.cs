5318732b28d04036e3258d8cd8edb731faeca103
Greg Whitfield [DATACOM] <Gregw@datacom.co.nz>
2019-02-25 21:37:53 +1300

    Adding response method for player menu (not working yet)

--- Assets/Scripts/PlayerMainMenu.cs
+++ Assets/Scripts/PlayerMainMenu.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerMainMenu : MonoBehaviour {
 
     //Main menu for player view, mainly only a bunch of callback called by the UI (setup throught the Inspector)
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   public void OnEnable ()
+    {
+        // Menu initialisation
+    }
+
+    public void OnClickFindMe ()
+    {
+        // Change player avatar to highlight position.
+        // Could also temporarily show the player name.
+        // TODO: find player object and call a method on that to do the work.
+
+        Debug.Log("Find Me! Clicked");
+    }
+
+    public void OnClickBack ()
+    {
+        // Returns back to lobby
+        // TODO - should be able to grab code from lobby example.
+
+    }
 }
