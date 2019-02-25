e048937927a3fade52328d858ea8b40a22ad6339
JamesDCross <jamesdcrossgithub@gmail.com>
2019-02-25 20:35:30 +1300

    Revert "latest"
    This reverts commit ec58932422393e9ac425fa6b9bcc1d5fa8648be3.

--- Assets/Scripts/AutoNPCSpawner.cs
+++ Assets/Scripts/AutoNPCSpawner.cs
@@ -1,43 +1,80 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
-public class AutoNPCSpawner : NetworkBehaviour {
-	public GameObject autoNPCPrefab;
-	public int numberOfNPCs;
-	public int spawnGroupSize = 10;  // How many to spawn at once
-	public float waitBeforeSpawn = 5.0f;
-	public float spawnGroupWait = 5.0f;
+public class AutoNPCSpawner : NetworkBehaviour
+{
+    public GameObject autoNPCPrefab;
+    public int numberOfNPCs;
+    public int spawnGroupSize = 10;  // How many to spawn at once
+    public float waitBeforeSpawn = 5.0f;
+    public float spawnGroupWait = 5.0f;
 
-	private int currentNPCCount = 0;
+    private int currentNPCCount = 0;
+    //temp
+    public List<GameObject> players; 
 
-	public override void OnStartServer()
-	{
+    public override void OnStartServer()
+    {
         // Rather than spawn all at once, we do a few at a time.
         if (numberOfNPCs > 0)
         {
             InvokeRepeating("SpawnNPCs", waitBeforeSpawn, spawnGroupWait);
         }
-	}
+    }
 
-	void SpawnNPCs () {
-		// Randomly spawns a set number of NPC's until we have reached a limit
-		for (int i = 0; i < spawnGroupSize; i++) {
-			var spawnPosition = new Vector3 (
-				Random.Range (-40.0f, 40.0f),
-				0.0f,
-				Random.Range (-40.0f, 40.0f));
-			var spawnRotation = Quaternion.Euler (
-				0.0f,
-				Random.Range (0, 180),
-				0.0f);
-			var npc = (GameObject.Instantiate (autoNPCPrefab, spawnPosition, spawnRotation));
-			NetworkServer.Spawn (npc);
-			currentNPCCount++;
-		}
-		if (currentNPCCount >= numberOfNPCs) {
-			CancelInvoke ("SpawnNPCs");
-		}
-	}
+    void SpawnNPCs()
+    {
+        // Randomly spawns a set number of NPC's until we have reached a limit
+        for (int i = 0; i < spawnGroupSize; i++)
+        {
+            var spawnPosition = new Vector3(
+                Random.Range(-40.0f, 40.0f),
+                0.0f,
+                Random.Range(-40.0f, 40.0f));
+            var spawnRotation = Quaternion.Euler(
+                0.0f,
+                Random.Range(0, 180),
+                0.0f);
+            var npc = (GameObject.Instantiate(autoNPCPrefab, spawnPosition, spawnRotation));
+            NetworkServer.Spawn(npc);
+            //temp
+            players.Add(npc);
+
+            currentNPCCount++;
+        }
+        if (currentNPCCount >= numberOfNPCs)
+        {
+            CancelInvoke("SpawnNPCs");
+        }
+    }
+    //temp
+    void Update()
+    {
+        // temp change player line connector (all)
+        if (Input.GetKey(KeyCode.C))
+        {
+            foreach (GameObject g in players)
+            {
+                g.GetComponent<PlayerConnector>().selectLinePrefab = 1;
+            }
+        }
+        if (Input.GetKey(KeyCode.V))
+        {
+            foreach (GameObject g in players)
+            {
+                g.GetComponent<PlayerConnector>().selectLinePrefab = 0;
+            }
+        }
+        if (Input.GetKey(KeyCode.B))
+        {
+            foreach (GameObject g in players)
+            {
+                g.GetComponent<PlayerConnector>().selectLinePrefab = 2;
+            }
+        }
+    }
+
+  
 }
-
 
